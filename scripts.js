96df4d0f9634abcae4c245b29467a999efdecddc
Jessica Slusark <slusark.jessica@gmail.com>
2023-02-17 15:43:55 +0100

    Added an array of objects to project

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
-alert("Hello world");
-let favouriteFood = `sushi`;
-document.write(favouriteFood);
+let pokemonList = [
+  { name: `Bulbasaur`, type: [`grass`, `poison`], height: `0.7m` },
+  { name: `Charmander`, type: `fire`, height: `0.6m` },
+  { name: `Squirtle`, type: `water`, height: `0.5m` },
+];
