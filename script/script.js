8c69bf39deb5284bd7f451ebd8ba344c80fed14b
Jessica Slusark <slusark.jessica@gmail.com>
2023-02-24 16:18:43 +0100

    Wrapped pokemonList into an IIFE

--- script/script.js
+++ script/script.js
@@ -1,15 +1,33 @@
-let pokemonList = [
-  { name: `Bulbasaur`, type: [`grass`, `poison`], height: 0.7 },
-  { name: `Charizard`, type: [`fire`, `flying`], height: 1.7 },
-  { name: `Squirtle`, type: [`water`], height: 0.5 },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: `Bulbasaur`, type: [`grass`, `poison`], height: 0.7 },
+    { name: `Charizard`, type: [`fire`, `flying`], height: 5.7 },
+    { name: `Squirtle`, type: [`water`], height: 0.5 },
+  ];
+
+  return {
+    getAll: function () {
+      return pokemonList;
+    },
+    add: function (pokemonData) {
+      pokemonList.push(pokemonData);
+    },
+  };
+})();
+
+pokemonRepository.add({
+  name: `Jigglypuff`,
+  type: [`normal`, `fairy`],
+  height: 1.8,
+});
 
 function writePokemon(pokemon) {
-  if (pokemon.height > 1) {
+  if (pokemon.height > 5) {
     document.write(`<li> ${pokemon.name} ${pokemon.height} `);
     document.write(`<span class="exclamation">- Wow! That's big!</span></li>`);
   } else {
+    //else
     document.write(`<li> ${pokemon.name} ${pokemon.height}`);
   }
 }
-pokemonList.forEach(writePokemon);
+pokemonRepository.getAll().forEach(writePokemon);
